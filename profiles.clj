67bfe8a683722c1630e88120ef143ebf6489dda2
Pervez Iqbal <pervezfunctor@gmail.com>
2015-05-11 21:26:56 +0530

    lein plugins - midje, try, oneoff, cprint, test refresh

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,9 @@
 {:user {
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  [lein-midje "3.1.3"]]
+                  [lein-midje "3.1.3"]
+                  [lein-exec "0.3.4"]
+                  [com.jakemccrary/lein-test-refresh "0.9.0"]
+                  [lein-try "0.4.3"]
+                  [lein-oneoff "0.3.1"]
+                  [lein-cprint "1.0.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}

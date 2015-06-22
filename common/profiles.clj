67ec5510723baae8fd085e26d2ca71ca459ed932
Pervez Iqbal <pervezfunctor@gmail.com>
2015-06-22 21:33:28 +0530

    add some lein plugins to experiment

--- common/profiles.clj
+++ common/profiles.clj
@@ -1,9 +1,18 @@
 {:user {
         :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [refactor-nrepl "1.0.5"]
+                  [com.jakemccrary/lein-test-refresh "0.10.0"]
+                  [lein-ancient "0.6.7"]
+                  [jonase/eastwood "0.2.1"]
                   [lein-midje "3.1.3"]
-                  [lein-exec "0.3.4"]
-                  [com.jakemccrary/lein-test-refresh "0.9.0"]
+                  [lein-exec "0.3.5"]
                   [lein-try "0.4.3"]
                   [lein-oneoff "0.3.1"]
-                  [lein-cprint "1.0.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+                  [lein-cprint "1.1.0"]
+                  [quickie "0.3.6"]
+                  [venantius/ultra "0.3.3"]]
+        :ultra {:color-scheme :solarized_dark}
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]
+                       [org.clojure/tools.nrepl "0.2.10"]]}}

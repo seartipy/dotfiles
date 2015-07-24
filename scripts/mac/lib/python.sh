a0c43e3ecccce9db1a68ea9f86994842aa50cc1b
Your Full Name <Your EMail Id>
2015-07-24 23:31:52 +0530

    remove exec shell from python script

--- scripts/mac/lib/python.sh
+++ scripts/mac/lib/python.sh
@@ -5,7 +5,6 @@ echo 'eval "$(pyenv init -)"' >> ~/.zshenv
 git clone https://github.com/yyuu/pyenv-pip-rehash.git ~/.pyenv/plugins/pyenv-pip-rehash
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshenv
 
-exec $SHELL
 pyenv install anaconda3-2.2.0
 pyenv global anaconda3-2.2.0
 pip install --upgrade pip

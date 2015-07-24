80b711b228facdb4db54dc32c20f1315ac39a7f8
Your Full Name <Your EMail Id>
2015-07-24 13:28:23 +0530

    update lein plugin versions

--- common/profiles.clj
+++ common/profiles.clj
@@ -1,6 +1,6 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.9.1"]
-                  [refactor-nrepl "1.0.5"]
+        :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "1.1.0"]
                   [com.jakemccrary/lein-test-refresh "0.10.0"]
                   [lein-midje "3.1.3"]
                   [lein-exec "0.3.5"]

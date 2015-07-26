ed3cc0d011fd62d693aac515df367b7dc964e91e
Your Full Name <Your EMail Id>
2015-07-26 13:51:30 +0530

    fix gnome settings

--- scripts/linux/gnome-settings.sh
+++ scripts/linux/gnome-settings.sh
@@ -3,7 +3,7 @@ gsettings set org.gnome.desktop.interface scaling-factor 2
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gdk/WindowScalingFactor': <2>}"
 
 gsettings set  org.gnome.desktop.screensaver lock-enabled false
-gsettings get  org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
+gsettings set  org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
 [ -e ~/.config/gtk-3.0/settings.ini ] || printf "[Settings]\ngtk-application-prefer-dark-theme=1\n" > ~/.config/gtk-3.0/settings.ini
 gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
 gsettings set org.gnome.Terminal.Legacy.Settings dark-theme true
@@ -18,5 +18,5 @@ gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "@as
 gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Alt>Tab']"
 gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver ''
 
-gsettings get org.gnome.desktop.privacy remove-old-temp-files true
-gsettings get org.gnome.desktop.privacy remove-old-trash-files true
+gsettings set org.gnome.desktop.privacy remove-old-temp-files true
+gsettings set org.gnome.desktop.privacy remove-old-trash-files true

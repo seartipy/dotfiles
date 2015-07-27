065c5da8c24f8c0e949d9acc53e5332ae17c7bd9
Your Full Name <Your EMail Id>
2015-07-27 11:26:08 +0530

    update leiningen plugin versions

--- common/profiles.clj
+++ common/profiles.clj
@@ -1,14 +1,14 @@
 {:user {
         :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "1.1.0"]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.10.0"]
                   [lein-midje "3.1.3"]
                   [lein-exec "0.3.5"]
                   [lein-try "0.4.3"]
                   [lein-oneoff "0.3.1"]
                   [lein-cprint "1.1.0"]
-                  [quickie "0.3.6"]
-                  [venantius/ultra "0.3.3"]]
+                  [quickie "0.4.0"]
+                  [venantius/ultra "0.3.4"]]
         :ultra {:color-scheme :solarized_dark}
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
-                       [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]}}
+                       [acyclic/squiggly-clojure "0.1.3-SNAPSHOT"]]}}

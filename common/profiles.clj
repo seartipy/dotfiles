9820e544bc4cc08dc840b7619e3e7956ae15cd0d
Your Full Name <Your EMail Id>
2015-08-16 21:05:35 +0530

    update lein plugins; add ancient to upgrade lein plugins

--- common/profiles.clj
+++ common/profiles.clj
@@ -2,13 +2,14 @@
         :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [refactor-nrepl "1.2.0-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.10.0"]
-                  [lein-midje "3.1.3"]
+                  [lein-midje "3.1.4-SNAPSHOT"]
                   [lein-exec "0.3.5"]
                   [lein-try "0.4.3"]
                   [lein-oneoff "0.3.1"]
                   [lein-cprint "1.1.0"]
                   [quickie "0.4.0"]
-                  [venantius/ultra "0.3.4"]]
+                  [venantius/ultra "0.3.4"]
+                  [lein-ancient "0.6.8-SNAPSHOT"]]
         :ultra {:color-scheme :solarized_dark}
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
-                       [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]]}}
+                       [acyclic/squiggly-clojure "0.1.3-SNAPSHOT"]]}}

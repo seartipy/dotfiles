2f53529b354375cc566e67da518fce0835438e27
Pervez Iqbal <pervezfunctor@gmail.com>
2015-08-22 20:49:53 +0530

    make linux setup files, mac setup files too

--- scripts/installer/mac/cpp-setup.sh
+++
@@ -1,8 +0,0 @@
-source $DOTFILES/mac/lib/base.sh
-source $DOTFILES/mac/lib/essential.sh
-
-emacs&
-
-source $DOTFILES/mac/lib/cpp.sh
-
-chsh -s /bin/zsh
--- scripts/installer/mac/ruby-setup.sh
+++
@@ -1,8 +0,0 @@
-source $DOTFILES/mac/lib/base.sh
-source $DOTFILES/mac/lib/essential.sh
-
-emacs&
-
-source $DOTFILES/mac/lib/ruby.sh
-
-chsh -s /bin/zsh

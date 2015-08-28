80681011b281d386c02afd84815339643096b6b8
Pervez Iqbal <pervezfunctor@gmail.com>
2015-08-28 17:38:36 +0530

    install the default boost version
    Hopefully allows you to use the same config for different ubuntu versions

--- scripts/installer/linux/cpp.sh
+++ scripts/installer/linux/cpp.sh
@@ -2,7 +2,7 @@ sudo apt-get install -y clang
 sudo apt-get install -y libclang-dev
 sudo apt-get install -y libc++-dev
 sudo apt-get install -y libc++abi-dev
-sudo apt-get install -y libboost1.55-all-dev
+sudo apt-get install -y libboost-all-dev
 sudo apt-get install -y g++
 sudo apt-get install -y cppcheck
 sudo apt-get install -y scons

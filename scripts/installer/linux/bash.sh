315c67bc29f25d32f410eec1c472fe1a5228b140
Pervez Iqbal <pervezfunctor@gmail.com>
2015-08-30 05:45:33 -0700

    source .bash_profile in .bashrc in linux

--- scripts/installer/linux/bash.sh
+++ scripts/installer/linux/bash.sh
@@ -1,3 +1,7 @@
-sclone https://github.com/nojhan/liquidprompt.git $SEARTIPY_HOME/vendors/liquidprompt
-smv ~/.bash_profile ~/.bash_profile-backup
-sln $SEARTIPY_HOME/dotfiles/common/bashrc ~/.bash_profile
+# sclone https://github.com/nojhan/liquidprompt.git $SEARTIPY_HOME/vendors/liquidprompt
+# smv ~/.bash_profile ~/.bash_profile-backup
+# sln $SEARTIPY_HOME/dotfiles/common/bashrc ~/.bash_profile
+
+if ! grep .bash_profile ~/.bashrc > /dev/null; then
+    echo "[ -f ~/.bash_profile ] && source ~/.bash_profile" >> ~/.bashrc
+fi

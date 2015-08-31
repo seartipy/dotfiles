979b3f4ba6085bfc74ebca6a89a82c229cf8de45
Pervez Iqbal <pervezfunctor@gmail.com>
2015-08-31 04:31:46 -0700

    fix ruby installer

--- scripts/installer/linux/ruby.sh
+++ scripts/installer/linux/ruby.sh
@@ -1,7 +1,9 @@
-if [ ! $(which rvm) ] || [ ! rvm list rubies 2> /dev/null | grep "=* ruby" ]; then
-    gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-    curl -sSL https://get.rvm.io | bash -s stable --ruby
-    source ~/.rvm/scripts/rvm
+if [ ! $(which rvm) ]; then
+    if ! rvm list rubies 2> /dev/null | grep "=* ruby"; then
+        gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+        curl -sSL https://get.rvm.io | bash -s stable --ruby
+        source ~/.rvm/scripts/rvm
+    fi
 fi
 
 gem install pry --no-ri --no-rdoc

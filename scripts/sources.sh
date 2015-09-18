70e2ca835676b83fe5792a3e2c96a6af9634baed
Pervez Iqbal <pervezfunctor@gmail.com>
2015-09-18 17:37:05 +0530

    fix merge conflict

--- scripts/sources.sh
+++ scripts/sources.sh
@@ -5,7 +5,7 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 #python
-if command pyenv 2>& /dev/null; then
+if pyenv --version > /dev/null 2> /dev/null ; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
 fi

4485658916f8772ccf6f6b557b7ba67cfe8f92e1
Pervez Iqbal <pervezfunctor@gmail.com>
2015-11-12 17:30:21 +0530

    update tools.nrepl version

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -12,5 +12,5 @@
                   [alembic "0.3.2"] ; A library for distilling dependencies
                   [lein-ancient "0.6.8"]] ; upgrade packages
         :ultra {:color-scheme :solarized_dark}
-        :dependencies [[org.clojure/tools.nrepl "0.2.11"]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [acyclic/squiggly-clojure "0.1.4"]]}} ; flycheck using eastwood

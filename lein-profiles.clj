1665d05f36e65b56a360b1b3c65a9cfcf64ee338
Pervez Iqbal <pervezfunctor@gmail.com>
2015-12-04 18:07:00 +0530

    update lein plugin versions

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -6,9 +6,9 @@
                   [lein-exec "0.3.5"] ; run clojure scripts
                   [lein-try "0.4.3"] ; try clojure library
                   [lein-oneoff "0.3.2"] ; run single clojure script
-                  [lein-cprint "1.1.0"] ; Like lein-pprint, but with colorization!
+                  [lein-cprint "1.2.0"] ; Like lein-pprint, but with colorization!
                   [quickie "0.4.1"] ; Lein plugin for auto testing
-                  [venantius/ultra "0.3.4"] ; A Leiningen plugin for a superior development environment
+                  [venantius/ultra "0.4.0"] ; A Leiningen plugin for a superior development environment
                   [alembic "0.3.2"] ; A library for distilling dependencies
                   [lein-ancient "0.6.8"]] ; upgrade packages
         :ultra {:color-scheme :solarized_dark}

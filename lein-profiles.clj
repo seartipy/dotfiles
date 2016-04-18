166623cc15877e51dd9b4e4190a4d63383b0abf7
Pervez Iqbal <pervezfunctor@gmail.com>
2016-04-18 12:22:38 +0530

    update sbt, lein, boot profiles

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,22 +1,22 @@
 {:user {
-        :plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"] ; nrepl server for cider in emacs
-                  [refactor-nrepl "2.0.0-SNAPSHOT"] ; refactor in emacs using clj-refactor
+        :plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"] ; nrepl server for cider in emacs
+                  [refactor-nrepl "2.3.0-SNAPSHOT"] ; refactor in emacs using clj-refactor
                   [com.jakemccrary/lein-test-refresh "0.11.0"]
                   [lein-midje "3.2"] ; midje plugin for continous testing
-                  [lein-exec "0.3.5"] ; run clojure scripts
+                  [lein-exec "0.3.6"] ; run clojure scripts
                   [lein-try "0.4.3"] ; try clojure library
                   [lein-oneoff "0.3.2"] ; run single clojure script
                   [lein-cprint "1.2.0"] ; Like lein-pprint, but with colorization!
-                  [quickie "0.4.1"] ; Lein plugin for auto testing
-                  [venantius/ultra "0.4.0"] ; A Leiningen plugin for a superior development environment
+                  [quickie "0.4.2"] ; Lein plugin for auto testing
+                  [venantius/ultra "0.4.1"] ; A Leiningen plugin for a superior development environment
                   [alembic "0.3.2"] ; A library for distilling dependencies
-                  [lein-ancient "0.6.8"]] ; upgrade packages
+                  [lein-ancient "0.6.10"]] ; upgrade packages
         :ultra {:color-scheme :solarized_dark}
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [spyscope "0.1.5"] ; lodash tap like macro
-                       [im.chit/vinyasa "0.4.2"]
-                       [io.aviso/pretty "0.1.20"]
-                       [acyclic/squiggly-clojure "0.1.4"]] ; flycheck using eastwood
+                       [im.chit/vinyasa "0.4.3"]
+                       [io.aviso/pretty "0.1.26"]
+                       [acyclic/squiggly-clojure "0.1.5"]] ; flycheck using eastwood
         :injections [(require 'spyscope.core)
                      (require 'io.aviso.repl)
                      (require '[vinyasa.inject :as inject])

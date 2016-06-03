c06a5ec789ec2054ab8142af7989f857e02ddc29
Pervez Iqbal <pervezfunctor@gmail.com>
2016-06-03 08:57:59 +0530

    add zgen options template

---
+++ templates/zgen-options-local.sh
@@ -0,0 +1,10 @@
+ZGEN_SYNTAX_HIGHLIGHTING="yes"
+ZGEN_AUTO_SUGGESIONS="no"
+
+# uncomment the following line, if you want to start tmux by default
+zgen prezto tmux:auto-start local 'no'
+# zgen prezto tmux:auto-start remote 'yes'
+
+# select your prompt, if you have powerline patched fonts installed,
+# and selected as terminal font, agnoster is a good option
+zgen prezto prompt theme 'agnoster'

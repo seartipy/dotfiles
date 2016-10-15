c6ecdcba1b21150f26ce2fe9e867f3b14d87cc9c
Pervez Iqbal <pervezfunctor@gmail.com>
2016-10-15 20:09:27 +0530

    update desktop installer

--- scripts/desktop-installer.sh
+++ scripts/desktop-installer.sh
@@ -6,6 +6,10 @@ common_install() {
     sudo apt install --no-install-recommends arc-theme ttf-ubuntu-font-family policykit-1
 }
 
+i3_install() {
+    sudo apt install --no-install-recommends i3 i3status
+}
+
 lxde_install() {
     sudo apt install --no-install-recommends lxde-core lxsession-logout lxde-common lxdm obconf lxappearance arc-theme ttf-ubuntu-font-family lxterminal lxpolkit
 }
@@ -17,7 +21,26 @@ xfce_install() {
 
 mate_install() {
     # might need to install mate-applets-common at least, may want mate-control-center too
-    sudo apt install --no-install-recommends mate-desktop mate-panel mate-polkit mate-session-manager mate-settings-daemon dconf-editor slim
+    # you need to set window manager, as marco is not installed
+    sudo apt install --no-install-recommends mate-desktop mate-panel mate-polkit mate-session-manager mate-settings-daemon dconf-editor slim mate-terminal
+}
+
+mate_recommended_install() {
+    # might need to install mate-applets-common at least, may want mate-control-center too
+    # you need to set window manager, as marco is not installed
+    sudo apt install --no-install-recommends mate-desktop mate-panel mate-polkit mate-session-manager mate-settings-daemon dconf-editor slim mate-terminal mate-control-center mate-applet-topmenu tilda
+}
+
+budgie_install() {
+    sudo apt install --no-install-recommends budgie-core gdm gnome-terminal
+}
+
+gnome_install() {
+    sudo apt install --no-install-recommends gnome-session gdm gnome-terminal
+}
+
+gnome_recommended_install() {
+    sudo apt install --no-install-recommends gnome-session gdm gnome-terminal gnome-tweak-tool gnome-control-center
 }
 
 is_yakkety || err_exit "currently only Ubuntu 16.10 supported"

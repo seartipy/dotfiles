d76f10c5c38e8c525d19cfd41a9015537162755d
Pervez Iqbal <pervezfunctor@gmail.com>
2017-05-05 14:58:12 +0530

    update leiningen plugins

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,8 +1,6 @@
 {:user {
-        :plugins [[com.jakemccrary/lein-test-refresh "0.18.0"]
-                  [lein-try "0.4.3"]
-                  [venantius/ultra "0.5.0"]
+        :plugins [[lein-try "0.4.3"]
+                  [venantius/ultra "0.5.1"]
                   [lein-ancient "0.6.10"]]
-        :dependencies [[spyscope "0.1.5"]
-                       [acyclic/squiggly-clojure "0.1.6"]]
+        :dependencies [[spyscope "0.1.5"]]
         :injections [(require 'spyscope.core)]}}

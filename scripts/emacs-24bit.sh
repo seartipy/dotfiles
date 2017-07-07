dadff2a557f375e6eaf16e13a0a39e594dded698
Pervez Iqbal <pervezfunctor@gmail.com>
2017-07-07 20:55:32 +0530

    fix emacs-24bit.sh

--- scripts/emacs-24bit.sh
+++ scripts/emacs-24bit.sh
@@ -5,12 +5,16 @@ source "$HOME/seartipy/dotfiles/scripts/utils.sh"
 add_emacs_ppa() {
     is_ubuntu || return 1
     ppa_exists elisp && return 0
+    echo "Adding PPA for emacs snapshot"
     sudo add-apt-repository ppa:ubuntu-elisp/ppa
 }
 
 emacs_snapshot_mac_install() {
     is_mac || return 1
+    echo "Trashing /Applications/Emacs.app, if present"
     srm /Applications/Emacs.app
+
+    echo "Installing emacs"
     brew tap caskroom/versions
     brew cask install emacs-nightly
 }
@@ -19,10 +23,13 @@ emacs_snapshot_ubuntu_install() {
     has_cmd emacs-snapshot && return 0
     add_emacs_ppa
     sudo apt-get update
+
+    echo "Installing emacs-snapshot"
     sudo apt-get install -y emacs-snapshot
 }
 
 emacs_snapshot_check() {
+    echo "Diagnosing..."
     if is_ubuntu; then
      has_cmd emacs-snapshot
      file_check ~/.terminfo/x/xterm-24bit
@@ -31,18 +38,20 @@ emacs_snapshot_check() {
         dir_check ~/.terminfo
         file_check /Applications/Emacs.app
     fi
+    echo "Diagnosing Done!"
 }
 
 emacs_snapshot_install() {
-    emacs_snaphost_mac_install
+    emacs_snapshot_mac_install
     emacs_snapshot_ubuntu_install
 }
 
 main() {
-    if ! is_ubuntu || ! is_mac; then
+    if ! is_ubuntu && ! is_mac; then
         echo "Only mac and ubuntu supported"
     fi
 
+    echo "generating terminfo"
     tic -x -o ~/.terminfo ~/seartipy/dotfiles/templates/terminfo-24bit.src
 
     emacs_snapshot_install

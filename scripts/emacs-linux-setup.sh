fa09338692cb3bbd4525626776c6b89375b850ec
Pervez Iqbal <pervezfunctor@gmail.com>
2017-07-26 22:20:35 -0700

    emacs desktop shortcuts, update for emacs25

--- scripts/emacs-linux-setup.sh
+++ scripts/emacs-linux-setup.sh
@@ -1,43 +1,47 @@
-alias srm=rm -f
+#! /usr/bin/env bash
 
-create_emacs_desktops() {
+source "$HOME/seartipy/dotfiles/scripts/utils.sh"
 
-    srm ~/.local/share/applications/emacsclient.emacs24-term.desktop
+seartipy_create_emacs_desktops() {
+    smd ~/.local/share/applications
 
-    cat <<EOF > ~/.local/share/applications/emacsclient.emacs24-term.desktop
+    srm ~/.local/share/applications/emacsclient.emacs25-term.desktop
+    slog "Creating emacsclient terminal .desktop"
+    cat <<EOF > ~/.local/share/applications/emacsclient.emacs25-term.desktop
 [Desktop Entry]
 Version=1.0
-Name=GNU Emacs Client 24 (Terminal)
+Name=Emacs Client 25 (Terminal)
 GenericName=Text Editor
 Comment=GNU Emacs is an extensible, customizable text editor - and more
 MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
-TryExec=/usr/bin/emacs24
-Exec=/usr/bin/emacsclient.emacs24 -t %F
-Icon=emacs24
+TryExec=/usr/bin/emacs25
+Exec=/usr/bin/emacs25 -t %F
+Icon=emacs25
 Type=Application
 Terminal=true
 Categories=Utility;Development;TextEditor;
 Keywords=Text;Editor;
-
 EOF
 
-    srm ~/.local/share/applications/emacsclient.emacs24.desktop
-    cat <<EOF > ~/.local/share/applications/emacsclient.emacs24.desktop
+    srm ~/.local/share/applications/emacsclient.emacs25.desktop
+    slog "Creating emacsclient terminal .desktop"
+    cat <<EOF > ~/.local/share/applications/emacsclient.emacs25.desktop
 [Desktop Entry]
 Version=1.0
-Name=GNU Emacs Client 24 (GUI)
+Name=EmacsClient 25 (GUI)
 GenericName=Text Editor
 Comment=GNU Emacs is an extensible, customizable text editor - and more
 MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
-TryExec=/usr/bin/emacs24
-Exec=/usr/bin/emacsclient.emacs24 -c -n %F
-Icon=emacs24
+TryExec=/usr/bin/emacs25
+Exec=/usr/bin/emacs25 -c -n %F
+Icon=emacs25
 Type=Application
 Terminal=false
 Categories=Utility;Development;TextEditor;
-StartupWMClass=Emacs24
+StartupWMClass=Emacs
 Keywords=Text;Editor;
-
 EOF
 
 }
+
+seartipy_create_emacs_desktops

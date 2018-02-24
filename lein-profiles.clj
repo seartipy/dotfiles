c62dfb8fbad959e27fb82ac8329e5b4933f3bab1
Pervez Iqbal <pervezfunctor@gmail.com>
2018-02-24 22:47:23 +0530

    update lein-ancient version

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,6 +1,6 @@
 {:user {
         :plugins [[lein-try "0.4.3"]
                   [venantius/ultra "0.5.2"]
-                  [lein-ancient "0.6.10"]]
+                  [lein-ancient "0.6.15"]]
         :dependencies [[spyscope "0.1.6"]]
         :injections [(require 'spyscope.core)]}}

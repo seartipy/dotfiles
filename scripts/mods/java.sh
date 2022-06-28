59de131b1ab3440524a362f8266673d526f4f583
Pervez Iqbal <pervezfunctor@gmail.com>
2022-06-29 02:48:00 +0530

    java installer

--- scripts/mods/java.sh
+++ scripts/mods/java.sh
@@ -45,69 +45,7 @@ java_install() {
     # has_cmd groovy || sdk install groovy < /dev/null
 }
 
-
-linux_lein_install() {
-    is_linux || return 1
-    has_cmd ~/bin/lein && return 1
-
-    slog "Installing leiningen"
-    curl -L https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > ~/bin/lein
-    chmod +x ~/bin/lein
-}
-
-clojure_mac_install() {
-    is_mac || return 1
-
-    slog "Installing clojure packages"
-    brew install leiningen
-}
-
-clojure_install() {
-    jdk_install
-
-    clojure_mac_install
-    linux_lein_install
-}
-
-
-scala_install() {
-    has_cmd coursier && return 1
-    jdk_install
-
-    slog "Install scala"
-    if is_linux; then
-        curl -fL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > cs && chmod +x cs
-        ./cs setup
-    elif is_mac; then
-        brew install coursier/formulas/coursier && cs setup
-    fi
-    srm cs
-    path_export "$HOME/.local/share/coursier/bin"
-}
-
-
 java_check() {
     # source "$HOME/.sdkman/bin/sdkman-init.sh"
     has_cmd javac java # sdk groovy maven gradle
-}
-
-scala_check() {
-    cmd_check javac sbt scala amm coursier
-}
-
-clojure_check() {
-    cmd_check javac lein
-}
-
-
-install_all() {
-    [ -n "$JAVA" ] && java_install
-    [ -n "$CLOJURE" ] && clojure_install
-    [ -n "$SCALA" ] && scala_install
-}
-
-post_installer_check() {
-    [ -n "$JAVA" ] && dotfiles_check
-    [ -n "$CLOJURE" ] && clojure_check
-    [ -n "$SCALA" ] && scala_check
 }
